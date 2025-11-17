41124c87d18a5fef892c14bc54f29a697badc8bf
SamidullaevKozim <samidullaevkozim@gmail.com>
2025-11-17 19:04:07 +0500

    bug fixing

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -43,7 +43,7 @@ const Cart = () => {
             onClick={clearCart}
             className="mt-3 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
           >
-            {t("cleanCart")}
+            {t("clearCart")}
           </button>
         </div>
       )}
